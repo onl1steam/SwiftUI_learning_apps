8879d2aafecf25a3ba2e1dd3e18ba0660b3141ea
Artem Ryzhkov (intern) <onl1steam@gmail.com>
2021-06-02 01:26:45 +0300

    Add project card views, working with stacks

---
+++ Starter Project/Starter Project/CardView.swift
@@ -0,0 +1,44 @@
+//
+//  CardView.swift
+//  Starter Project
+//
+//  Created by Рыжков Артем on 02.06.2021.
+//
+
+import SwiftUI
+
+struct CardView: View {
+    var body: some View {
+        VStack {
+            HStack {
+                VStack {
+                    Text("UI Design")
+                        .font(.title)
+                        .fontWeight(.semibold)
+                        .foregroundColor(.white)
+                    Text("Certificate")
+                        .foregroundColor(Color("accent"))
+                }
+                Spacer()
+                Image("Logo1")
+            }
+            .padding(.horizontal, 20)
+            .padding(.top, 20)
+            Spacer()
+            Image("Card1")
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .frame(width: 300, height: 110, alignment: .top)
+        }
+        .frame(width: 340, height: 220)
+        .background(Color.black)
+        .cornerRadius(20)
+        .shadow(radius: 20)
+    }
+}
+
+struct CardView_Previews: PreviewProvider {
+    static var previews: some View {
+        CardView()
+    }
+}
---
+++ Starter Project/Starter Project/Starter_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Starter_ProjectApp.swift
+//  Starter Project
+//
+//  Created by Рыжков Артем on 02.06.2021.
+//
+
+import SwiftUI
+
+@main
+struct Starter_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
